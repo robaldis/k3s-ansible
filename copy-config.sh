987822d072a4111bf5edcf8261ee657fdd7eeba0
robaldis <robertaldis@gmail.com>
2023-04-12 21:37:10 +0100

    updating scripts

---
+++ copy-config.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+scp raldis@thor-1:~/.kube/config ~/.kube/config
+#scp raldis@thor-1:/etc/rancher/k3s/k3s.yaml ~/.kube/k3s.yaml
--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-ansible-playbook site.yml
+ansible-playbook site.yml -i inventory/thor/hosts.ini --ask-become-pass
